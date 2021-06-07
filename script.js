d8b35e0e249a705a30cd4535d7f3bd0071eeefa0
imzajjithvedha <zajjith@gmail.com>
2021-06-07 11:56:24 +0530

    table selection changed and nav added

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const select = document.querySelectorAll('.table-select');
 select[0].onchange = function () {
-    select[0].className = 'table-select ' + this.options[this.selectedIndex].className;
+    select[0].className = 'form-control table-select ' + this.options[this.selectedIndex].className;
 }
 select[1].onchange = function () {
-    select[1].className = 'table-select ' + this.options[this.selectedIndex].className;
+    select[1].className = 'form-control table-select ' + this.options[this.selectedIndex].className;
 }
 select[2].onchange = function () {
-    select[2].className = 'table-select ' + this.options[this.selectedIndex].className;
+    select[2].className = 'form-control table-select ' + this.options[this.selectedIndex].className;
 }
 select[3].onchange = function () {
-    select[3].className = 'table-select ' + this.options[this.selectedIndex].className;
+    select[3].className = 'form-control table-select ' + this.options[this.selectedIndex].className;
 }
 select[4].onchange = function () {
-    select[4].className = 'table-select ' + this.options[this.selectedIndex].className;
+    select[4].className = 'form-control table-select ' + this.options[this.selectedIndex].className;
 }
 select[5].onchange = function () {
-    select[5].className = 'table-select ' + this.options[this.selectedIndex].className;
+    select[5].className = 'form-control table-select ' + this.options[this.selectedIndex].className;
 }
